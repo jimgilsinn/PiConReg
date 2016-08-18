d9e21a9b6982b6c51ddf8b19e583e92afdbe3079
Jim Gilsinn <jimgilsinn@gmail.com>
2016-08-18 01:28:36 -0400

    Initial upload

---
+++ initialize_device.sh
@@ -0,0 +1,3 @@
+sudo apt-get install python-qrtools
+sudo pip install watchdog
+sudo pip install ConfigParser
---
+++ startup.sh
@@ -0,0 +1,4 @@
+sudo modprobe bcm2835-v4l2
+sudo rm /var/lib/motion/*.jpg /var/lib/motion/*.avi
+sudo motion
+
